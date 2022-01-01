f8883968001b8a5d670f8f73fb815a1d7237b78c
sensu-watson <sensunowatson@gmail.com>
2022-01-02 02:34:35 +0900

    バグ修正

--- docs/js/header_html_output.js
+++ docs/js/header_html_output.js
@@ -1,13 +1,13 @@
 const xhr = new XMLHttpRequest(),
   method = "GET",
   url = "common_html/header.html";
-const header = document.getElementById("header"); //読み込みたい位置を指定
+const header = document.getElementById("header");
 
 xhr.open(method, url, true);
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4 && xhr.status === 200) {
     const restxt = xhr.responseText;
-    document.write(restxt);
+    header.innerHTML = restxt;
   }
 };
 xhr.send();
